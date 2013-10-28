ed1942dfd11a1a4addf58d566c3c3ef787003435
Tom Armitage <tom@infovore.org>
2013-10-28 10:10:22 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instagrammatron::Application.config.secret_key_base = '21f52db701fe95a6c06e421c24b43075323162b9b6d9479d1f96217d977ab32926ba2eda7e2e338e7aa5beb556d9a67432d734ee7fefba8097bccc95721e0a7f'
